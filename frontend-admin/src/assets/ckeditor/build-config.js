827e0ab1db96cde2421ebc233833ffd42dc3fd27
minhducand2 <minhducand@gmail.com>
2021-11-28 22:36:38 +0700

    update

--- frontend-admin/src/assets/ckeditor/build-config.js
+++ frontend-admin/src/assets/ckeditor/build-config.js
@@ -3,26 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
-/**
- * This file was added automatically by CKEditor builder.
- * You may re-use it at any time to build CKEditor again.
- *
- * If you would like to build CKEditor online again
- * (for example to upgrade), visit one the following links:
- *
- * (1) https://ckeditor.com/cke4/builder
- *     Visit online builder to build CKEditor from scratch.
- *
- * (2) https://ckeditor.com/cke4/builder/ecb4079e05896b6dd78c6aaa4307979a
- *     Visit online builder to build CKEditor, starting with the same setup as before.
- *
- * (3) https://ckeditor.com/cke4/builder/download/ecb4079e05896b6dd78c6aaa4307979a
- *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
- *
- * NOTE:
- *    This file is not used by CKEditor, you may remove it.
- *    Changing this file will not change your CKEditor configuration.
- */
+
 
 var CKBUILDER_CONFIG = {
 	skin: 'office2013',
